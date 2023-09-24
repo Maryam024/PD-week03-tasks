5f061b219e826d69f4f11fff6668211b77b45bfd
Maryam024 <144409702+Maryam024@users.noreply.github.com>
2023-09-24 15:22:22 +0500

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main(){
+
+int minutes;
+cout<<"Number of minutes";
+cin>> minutes;
+int framespersecond;
+cout<<"Frames per second";
+cin>> framespersecond;
+int result;
+result= minutes*60*framespersecond;
+cout<<"Total number of frames: "<<result;
+
+
+
+
+}
