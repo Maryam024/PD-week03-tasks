dfc3864617f37f1b228eb62e6ca46e919162318c
Maryam024 <144409702+Maryam024@users.noreply.github.com>
2023-09-24 15:22:43 +0500

    Add files via upload

---
+++ task3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main(){
+int initialvelocity;
+cout<<"Enter initial velocity";
+cin>> initialvelocity;
+int acceleration;
+cout<<"Enter acceleration";
+cin>> acceleration;
+int acceleration;
+int time;
+cout<<"Enter time";
+cin>> time;
+int result;
+result=(acceleration*time)+initialvelocity;
+cout<<"Final velocity = "<<result;
+
+
+
+
+}
