99eea53bd0dd38a2c32209f0088228f854e25fe6
Maryam024 <144409702+Maryam024@users.noreply.github.com>
2023-09-24 15:23:31 +0500

    Add files via upload

---
+++ task5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main(){
+
+string name;
+cout<<"Enter the name of the Person :";
+cin>> name;
+int target_weightloss;
+cout<<"Enter the target weight loss in kilograms :";
+cin>> target_weightloss;
+int result;
+result= target_weightloss*15;
+cout<<name<<" will need "<<result<<" days to lose "<<target_weightloss<<"kg by following the doctor's suggestion ";
+
+
+
+
+}
