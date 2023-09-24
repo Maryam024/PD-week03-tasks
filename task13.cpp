8c421b83d798cb53bc30e80855dd12d512463521
Maryam024 <144409702+Maryam024@users.noreply.github.com>
2023-09-24 15:29:51 +0500

    Add files via upload

---
+++ task13.cpp
@@ -0,0 +1,27 @@
+  #include<iostream>
+using namespace std;
+
+int main()
+{
+int num;
+int sum(0);
+cout<<"type first integer number";
+cin>>num;
+sum+=num;
+cout<<"type second integer number";
+cin>>num;
+sum+=num;
+cout<<"type third integer number";
+cin>>num;
+sum+=num;
+cout<<"type fourth integer number";
+cin>>num;
+sum+=num;
+cout<<"type fifth integer number";
+cin>>num;
+sum+=num;
+cout<<"the sum of the number"<<endl;
+cout<<sum;
+return 0;
+}
+
