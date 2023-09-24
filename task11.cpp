55251e6a81c9f1149d966fc41119c0a37de81404
Maryam024 <144409702+Maryam024@users.noreply.github.com>
2023-09-24 15:26:04 +0500

    Add files via upload

---
+++ task11.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main(){
+
+int age;
+cout<<"Enter the Person's Age: ";
+cin>> age;
+int moves;
+cout<<"Enter the number of times they've moved: ";
+cin>> moves;
+int result;
+result= age/(moves+1);
+cout<<"Average number of years lived in the same house: "<<result;
+
+
+
+}
