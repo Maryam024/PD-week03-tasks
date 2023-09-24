6777097667e9cd4da961f0309b75b43e8bb74adc
Maryam024 <144409702+Maryam024@users.noreply.github.com>
2023-09-24 15:25:10 +0500

    Add files via upload

---
+++ task10.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+main(){
+
+int _n1;
+cout<<"Number 1: ";
+cin>> _n1;
+int _n2;
+cout<<"Number 2: ";
+cin>> _n2;
+int _n3;
+cout<<"Number 3: ";
+cin>> _n3;
+int _n4;
+cout<<"Number 4: ";
+cin>> _n4;
+int _n5;
+cout<<"Number 5: ";
+cin>> _n5;
+int _n6;
+cout<<"Number 6: ";
+cin>> _n6;
+int _n7;
+cout<<"Number 7: ";
+cin>> _n7;
+int _n8;
+cout<<"Number 8: ";
+cin>> _n8;
+int _n9;
+cout<<"Number 9: ";
+cin>> _n9;
+int _n10;
+cout<<"Number 10: ";
+cin>> _n10;
+int _n11;
+cout<<"Number 11: ";
+cin>> _n11;
+int _n12;
+cout<<"Number 12: ";
+cin>> _n12;
+int _n13;
+cout<<"Number 13: ";
+cin>> _n13;
+int _n14;
+cout<<"Number 14: ";
+cin>> _n14;
+int _n15;
+cout<<"Number 15: ";
+cin>> _n15;
+int add;
+add=_n1+_n2+_n3+_n4+_n5;
+int multiplication;
+multiplication=_n6*_n7*_n8*_n9*_n10;
+int subtract;
+subtract=_n11-_n12-_n13-_n14-_n15;
+int result;
+result=add+multiplication;
+int finalresult;
+finalresult=result-subtract;
+cout<<"Final result is: "<<finalresult;
+
+
+
+
+
+
+
+
+
+}
